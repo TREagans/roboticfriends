6a4eca11c86c2d00fc5ba9301a9d8cf192864b27
Unpredict2ble <unpredict2ble@gmail.com>
2021-03-01 13:03:09 -0600

    converted the app to use react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+
+import React, { useState, useEffect } from 'react';
+
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,55 +8,61 @@ import ErrorBoundary from '../components/ErrorBoundary';
 
 import './App.css';
 
-class App extends Component {
-	// state no longer requires constructor() or super()
-	state = {
-		robots: [],
-		searchField: ''
-	};
+const App = () => {
 
-	// runs when app loads
-	async componentDidMount() {
+	// hook state
+	const [robots, setRobots] = useState([]);
+	const [searchField, setSearchField] = useState('');
 
-		// making API call to JSON placeholder (users)
-		const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
-		// must also await JSON conversion
-		const users = await response.json();
+	// useEffect is used in place of lifecycle methods
+	// notice how we create an async function w/in then called
+	useEffect(() => {
 
-		// setting the state
-		this.setState({ robots: users});
-	}
+		const fetchBots = async () => {
+
+			// making API call to JSON placeholder (users)
+			const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+			// must also await JSON conversion
+			const users = await response.json();
+
+			// setting the state
+			setRobots(users);
+		}
+
+		// call the fetchBots function
+		fetchBots();
+
+	}, []);
 
 	// defining search change function
-	onSearchChange = evt => {
+	const onSearchChange = evt => {
+
 		// update search field state based on entered text
-		this.setState({ searchField: evt.target.value });
+		setSearchField(evt.target.value);
 	};
 
-	render() {
-		const { robots, searchField } = this.state;
 
-		// filter the robots based on search field text
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchField.toLowerCase());
-		});
+	// filter the robots based on search field text
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
+	});
 
-		// if load time takes long, display loading
-		return !robots.length ? (
-			<h1 className='tc'>Loading . . .</h1>
-		) : (
-			<div className='tc'>
-				<h1 className='f1'>Robotic Friends</h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<Scroll>
-					<ErrorBoundary>
-						<CardList robots={filteredRobots} />
-					</ErrorBoundary>
-				</Scroll>
-			</div>
-		);
-	}
+	// if load time takes long, display loading
+	return !robots.length ? (
+		<h1 className='tc'>Loading . . .</h1>
+	) : (
+		<div className='tc'>
+			<h1 className='f1'>Robotic Friends</h1>
+			<SearchBox searchChange={onSearchChange} />
+			<Scroll>
+				<ErrorBoundary>
+					<CardList robots={filteredRobots} />
+				</ErrorBoundary>
+			</Scroll>
+		</div>
+	);
 }
 
 export default App;
